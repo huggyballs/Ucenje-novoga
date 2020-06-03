f1ffc25f5708122d501265da1caaae2754f340f2
huggyballs <spomich123@gmail.com>
2020-06-03 14:53:10 +0200

    Create sintaksa.py

---
+++ sintaksa.py
@@ -0,0 +1,17 @@
+dodavanje = ''
+
+print("unosenje broja")
+dodavanje = input()
+
+if dodavanje is not '' :
+    print("Uspjeh!")
+else:
+    print("Neuspjeh!")
+
+print("unosenje drugog broja")
+dodavanje = input()
+
+if dodavanje != '' :
+    print("Uspjeh 2!")
+else:
+    print("Neuspjeh 2!")
