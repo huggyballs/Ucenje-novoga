33aaa7972b3ab60abeeb6ffaa412fd131908aa71
huggyballs <spomich123@gmail.com>
2020-06-09 12:33:55 +0200

    Create petljacitanje.py

---
+++ petljacitanje.py
@@ -0,0 +1,51 @@
+import nfc
+import time
+
+clf = nfc.ContactlessFrontend()
+clf.open('ttyAMA0')
+
+def main():
+    try:
+        while True:
+            unos = ''
+            print("Zelite pokusati citati? 1 - DA 3 - NE")
+            
+            while unos != 1 and unos != 3:
+
+                unos = input()
+
+                if input == 1:
+                    print("citanje")
+                    starttime = time.time()
+                    userID = ''
+
+                    while True:
+                        currenttime = time.time()
+                        elapsedtime = currenttime - starttime
+
+                        userID = clf.connect(rdwr={'on-connect': lambda tag: False})
+
+                        if elapsedtime > 5 :
+                            print("Neuspjesno citanje!")
+                            break
+                        elif userID != '' :
+                            print("Uspjesno citanje!")
+                            print(userID)
+                            break
+
+                    break
+                elif input == 3:
+                    print("nema citanja")
+                    break
+                else:
+                    print("neispravan unos!")
+                    pass
+                pass
+            pass
+        pass
+    except KeyboardInterrupt:
+        print("Kraj rograma!")
+        pass
+
+if __name__ == '__main__':
+    main()
