c6d4024dc7c1dd7630c3df6d153218d4084523ce
huggyballs <spomich123@gmail.com>
2020-06-26 13:43:33 +0200

    Update sql2.py

--- sql2.py
+++ sql2.py
@@ -3,32 +3,34 @@ from datetime import datetime
 
 db = mysql.connector.connect(
 	host="localhost",
-	user="marko",
-	passwd="marko",
-	database="testdatabese"
+	user="root",
+	passwd="raspberry",
+	database="practicedatabase"
 	)
 
 mycursor = db.cursor()
 
 try:
-	mycursor.execute("CREATE TABLE Users2 (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL)")
+	mycursor.execute("CREATE TABLE Users (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL)")
 	mycursor.execute("CREATE TABLE Devices (id2 int PRIMARY KEY NOT NULL AUTO_INCREMENT, userid int NOT NULL, deviceid VARCHAR(25))")
+	db.commit()
 	pass
 except:
 	print("Ne valja!")
+	db.commit()
 	pass
 
 print("Unesi ime: ")
 ime = input()
 
-mycursor.execute("INSERT INTO Users (name) VALUES (%s)", (ime))
+mycursor.execute("INSERT INTO Users (name) VALUES (%s)", ("Marko"))
 db.commit()
 
 last_id = mycursor.lastrowid
 print("Unesi id uredjaja: ")
 ur = input()
 
-mycursor.execute("INSERT INTO Devices (userid, deviceid) VALUES (%s, %s)", (last_id, ur))
+mycursor.execute("INSERT INTO Devices (userid, deviceid) VALUES (%s, %s)", (last_id, "1234"))
 db.commit()
 
 mycursor.execute("SELECT * FROM Users")
