f7e1bebff5c9744c4aadc2b967fe0c0d5bbf10a6
huggyballs <spomich123@gmail.com>
2020-07-28 12:00:25 +0200

    Update sintaksa.py

--- sintaksa.py
+++ sintaksa.py
@@ -15,3 +15,20 @@ if dodavanje != '' :
     print("Uspjeh 2!")
 else:
     print("Neuspjeh 2!")
+
+def Petlja():
+    while True:
+        odgovor = int(input("Unesi jedan ili dva"))
+        if odgovor == 1:
+            print("Unesena jedinica")
+            return False
+        elif odgovor == 2:
+            print("Unesena dvojka")
+            return False
+        else:
+            print("Nepravilan unos!")
+            pass
+        pass
+
+Petlja()
+print("Zavrsena petlja!")
